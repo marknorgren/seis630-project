58611962014adfbe8d0df08e7f8d3dabbeb9a9ed
Mark N <m@marknorgren.com>
2010-12-08 20:44:04 -0600

    updated with most recent edits
    Signed-off-by: Mark N <m@marknorgren.com>

--- projectQueries.sql
+++ projectQueries.sql
@@ -1,4 +1,4 @@
-Query 1
+--Query 1
 
 select rname, ename from relates where rname = (	
  	select rname from relates group by rname having count(rname)>2
@@ -7,14 +7,21 @@ select rname, ename from relates where rname = (
 
 ------------------------------------------------------------------------------------
 
-Query 2
+--Query 2
 
-select rname, ename from relates where rname = (	
- 	select rname from relates group by rname having count(rname)=3
-);
+select rname, ename 
+	from relates 
+	where rname = (	
+					select rname 
+					from relates 
+					group by rname 
+					having count(rname)=3
+	);
 
 --Extra Credit - Display this all on one line.
 ------------------------------------------------------------------------------------
+
+
 
 --Query 3 (need verification)
 --Print the name of all binary relationships that have a cardinality --of M-to-M and have max
@@ -24,29 +31,28 @@ select rname, ename from relates where rname = (
 select rname from relates group by rname having count(rname)=2;
 
 SELECT lower(RNAME)
- FROM   RELATES
- where upper(RL_CARD)='M'
- GROUP BY lower(RNAME)
- HAVING COUNT (lower(RNAME))=2;
+FROM   RELATES
+WHERE upper(RL_CARD)='M'
+GROUP BY lower(RNAME)
+HAVING COUNT (lower(RNAME))=2;
 
- SELECT DISTINCT R.RNAME
- FROM   RELATES RL, REL_TYPE R
- WHERE  LOWER(R.RTYPE)='binary'
- 	AND	LOWER(RL.RL_CARD)='m'
- 	AND	LOWER(RL.RL_MAX_CARD)='m'
- 	AND	RL.RNAME=R.RNAME;
+SELECT DISTINCT R.RNAME
+FROM   RELATES RL, REL_TYPE R
+WHERE LOWER(R.RTYPE)='binary'
+ 		AND	LOWER(RL.RL_CARD)='m'
+ 		AND	LOWER(RL.RL_MAX_CARD)='m'
+ 		AND	RL.RNAME=R.RNAME;
 
---
-select rname
-from relates
-group by rname
-having count(rname)=2
-AND lower(RL_MAX_CARD) = ‘m’
 --This top block finds all binary relationships
-minus
-select distinct rname
-from relates
-where lower(rl_card)<>'m';
+SELECT rname
+FROM relates
+WHERE lower(RL_MAX_CARD) = 'm'
+GROUP BY rname	
+HAVING count(rname)=2
+	MINUS
+		SELECT DISTINCT rname
+		FROM relates
+		WHERE lower(rl_card)<>'m';
 
 --This block finds relationships that are not M
 
@@ -55,11 +61,11 @@ where lower(rl_card)<>'m';
 ------------------------------------------------------------------------------------
 
 
-Query 4
+--Query 4
 --4. For entities that have two or three attributes, print the name --of the entity and its attributes.
-select ename, aname
-from contains
-where ename in( select ename
+SELECT ename, aname
+FROM contains
+WHERE ename IN( select ename
             	from contains
             	group by ename
             	having count(ename)=any(2,3)
@@ -69,40 +75,40 @@ order by ename;
 ------------------------------------------------------------------------------------
 
 
-Query 5
+--Query 5
 --5. Print the name of all entities that participate in 2, 3, or 5 relationships.
 select ename, rname
-from relates
-where ename in( select ename
-            	from relates
-            	group by ename
-            	having count(ename)=any(2,3,5)
-            	)
-order by ename;
+	from relates
+	where ename in( select ename
+					from relates
+					group by ename
+					having count(ename)=any(2,3,5)
+					)
+	order by ename;
 
 ------------------------------------------------------------------------------------
 
 
-Query 6
+--Query 6
 --6. Print the name of all entities that have at least one attributes with the data type “Date”
 select distinct ename
-from contains
-where lower(cn_datatype)='date';
+	from contains
+	where lower(cn_datatype)='date';
 
 ------------------------------------------------------------------------------------
 
 
-Query 7
+--Query 7
 --7. For all attributes that use a domain, print the name the attribute,
 --the name of the domain, and the data type of the domain (one line per attribute).
 select aname, cn_domain, cn_datatype
-from contains
-where lower(cn_domain) is not null
-order by aname;
+	from contains
+	where lower(cn_domain) is not null
+	order by aname;
 
 ------------------------------------------------------------------------------------
 
-Query 8
+--Query 8
 --8. Print the name of any attribute, entity, relationship, or specialization
 --that has either lowercase “r” or uppercase “R” somewhere in their name.
 --For example, if an entity has the name “Project”, your query will print a line
@@ -110,36 +116,48 @@ Query 8
 --also prints a line as “Refers 	Relationship”.
 ----------------------------
 select concat(ename,'Entity')
-From entity_type
-Where lower(ename) like '%r%'
+	From entity_type
+	Where lower(ename) like '%r%'
 union
 select concat(aname,'Attribute')
-From attr_type
-Where lower(aname) like '%r%'
+	From attr_type
+	Where lower(aname) like '%r%'
 union
 select concat(rname,'Relationship')
-From rel_type
-Where lower(rname) like '%r%'
+	From rel_type
+	Where lower(rname) like '%r%'
 union
-select concat(sname,'Specialization')
-From specialization
-Where lower(sname) like '%r%';
+	select concat(sname,'Specialization')
+		From specialization
+	Where lower(sname) like '%r%';
 
 --answer returns 17 rows
 
 ------------------------------------------------------------------------------------
 
 
-Query 9
+--Query 9
 --Print the name of entities that participate in at least 2 
---relationships with cardinality of 1 or 2
---relationships with cardinality M on the end that connects to the --entity.
-
+--relationships with cardinality of 1 or 
+--2 relationships with cardinality M on the end that connects to the --entity.
+select distinct ename 
+from relates
+where lower(rl_card)='1'
+group by ename
+having count (ename)>1 
+-- this selects entiry with two or more card 1
+union
+select distinct ename 
+from relates
+where lower(rl_card)='m'
+group by ename
+having count (ename)=2;  
+--this selects entities with two card M
 
 
 ------------------------------------------------------------------------------------
 
-Query 10
+--Query 10
 --10. Print the name of each entity that has a Pkey with the name(s)
 --of the attribute(s) that form the Pkey
 select ename, aname
@@ -149,7 +167,7 @@ order by ename;
 
 ------------------------------------------------------------------------------------
 
-Query 11
+--Query 11
 --11. Print the names of all derived and base attributes and the data type of the --each.
 --For example, if Age is derived from DOB and the data type of Age is Number(2)
 --and the data type of DOB is Date, you need to print: Age Number(2) DOB  Date
@@ -159,11 +177,15 @@ where dv.att_aname=cn.aname;
 
 ------------------------------------------------------------------------------------
 
-Query 12
+--Query 12
 
 --Print the name of all composite attributes and their component 
 --attributes for those composite attributes that have one component 
 --attributes is defined on a domain.
+select att_aname, aname
+from is_in
+where ii_domain is not null;
+
 
 ------------------------------------------------------------------------------------
 
